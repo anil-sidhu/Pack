ff8fb71761d8244991903dfd76127ad4c7044911
anil <webanilsidhu@gmail.com>
2018-12-15 23:53:52 +0530

    first app

---
+++ PackServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace anilsidhu\pack;
+
+use Illuminate\Support\ServiceProvider;
+
+class PackServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // include __DIR__.'/routes/web.php';
+    }
+
+
+}
---
+++ src/TestFile.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace anilsidhu\pack;
+
+/**
+ * Laravel htm2pdf: convert html into pdf
+ *
+ * @package laravel-html2pdf
+ * @author Nahidul Hasan <nahidul.cse@gmail.com>
+ */
+class TestFile
+{
+
+    /**
+     * Convert html into pdf
+     *
+     * @param $input
+     * @return bool|string
+     */
+    public static function testFunction($input)
+    {
+        echo "<h1>next</h1>";
+        return "some next";
+
+    }
+
+}
